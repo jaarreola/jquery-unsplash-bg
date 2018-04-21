ab9a2cddc09335097d9cc37f256bb0391ca79383
Jjimmy <jaarreola.p@gmail.com>
2018-04-21 15:04:14 -0500

    add jquery env

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-var sayHi = 'Hola mundo';
+/**
+ * jquery-unsplash-bg
+ * @version 0.1.0-development
+ * @author Jaime Arreola
+ * @license The MIT License (MIT)
+ */
+
+(function($) {
+  $.fn.setRed = function() {
+    this.css('background-color', 'red');
+    return this;
+  };
+})(jQuery);
